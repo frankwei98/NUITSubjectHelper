cef1f45023c7bc9dc98ab09acb1b09e7d92dba53
Frank Wei <frank@frankwei.xyz>
2017-10-09 07:54:34 +0800

    Rename project, use generator now

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
-# NUIT Subject Timetable Spider
+# NUIT Subject Helper
 
 ## 一些点子
 
 * 微信 Bot 推送新一天的课程安排 (只需要和 WXPY 整合)
 
-
+* 模拟登陆教务网
 
 ## TODO List
 
  - [ ] 微信机器人推送
 
- - [ ] 自动抓取抓取教务网
+ - [ ] 自动从教务网抓取课表
  
  - [ ] 转化为 iCalender 日历所用的 `.ics` 日历文件
 
--- search.py
+++ search.py
@@ -1,9 +1,18 @@
 from convert import Convert
 
-if __name__ == '__main__':
-    search_week = int(input('请输入第几周(阿拉伯数字)'))
+
+def search(week):
+    # result = []
     data = Convert('data.json').get_data()
     for item in data:
         for week in item['开课周']:
             if week == search_week:
-                print(item)
+                yield item
+                break
+    # return result
+
+
+if __name__ == '__main__':
+    search_week = int(input('请输入第几周(阿拉伯数字)'))
+    for item in list(search(search_week)):
+        print(item)
