692a0d89a02ece99dcfeb16bc3f73c9784d275c0
Frank Wei <frank@frankwei.xyz>
2017-10-09 09:24:05 +0800

    Add wechat bot instance, add time to str converter

--- README.md
+++ README.md
@@ -6,11 +6,11 @@
 
 * 模拟登陆教务网
 
-## TODO List
+## Todo List
 
- - [ ] 微信机器人推送
+ - [ ] 集成 [wxpy](https://github.com/youfou/wxpy) 微信机器人, 按天推送提醒课程安排
 
- - [ ] 自动从教务网抓取课表
+ - [ ] 自动从教务网抓取课表 (涉及到爬虫)
  
  - [ ] 转化为 iCalender 日历所用的 `.ics` 日历文件
 
--- search.py
+++ search.py
@@ -1,7 +1,7 @@
 from convert import Convert
 
 
-def search(week):
+def search(search_week):
     # result = []
     data = Convert('data.json').get_data()
     for lesson in data:
@@ -9,10 +9,16 @@ def search(week):
             if week == search_week:
                 yield lesson
                 break
-    # return result
+                # return result
 
 
 if __name__ == '__main__':
     search_week = int(input('请输入第几周(阿拉伯数字)'))
-    for item in list(search(search_week)):
+    result = list(search(search_week))
+    for item in result:
         print(item)
+
+    print('第 {0} 周一共有 {1} 节课'.format(
+        search_week,
+        len(result)
+    ))
